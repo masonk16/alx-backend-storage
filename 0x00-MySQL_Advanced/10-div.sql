1f9d747ac46cf58415da63ed6b99bfdeeeee364d
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-02-09 08:58:02 +0000

    11. No table for a meeting

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,6 +1,7 @@
 -- Creates a function SafeDiv that divides (and returns) the first by the second number or returns 0 if the second number is equal to 0.
+DROP FUNCTION IF EXISTS SafeDiv;
 DELIMITER $$
-CREATE FUNCTION IF NOT EXISTS SafeDiv (a INT, b INT)
+CREATE FUNCTION SafeDiv (a INT, b INT)
 RETURNS FLOAT DETERMINISTIC
 BEGIN
     DECLARE result FLOAT DEFAULT 0;
